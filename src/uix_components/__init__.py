4bc62f520f8ebdcc3c7a784e2fc9a5c9647739af
semiharslanait <semih@ait.com.tr>
2024-01-09 17:30:55 +0300

    Add basic_loading and basic_imagecard components

--- src/uix_components/__init__.py
+++ src/uix_components/__init__.py
@@ -1,3 +1,5 @@
 from ._basic_slider._basic_slider import basic_slider
 from ._component_list._component_list import component_list
 from ._basic_datalist._basic_datalist import basic_datalist
+from ._basic_loading._basic_loading import basic_loading
+from ._basic_imagecard._basic_imagecard import basic_imagecard
