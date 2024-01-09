47c9a55c8eda512bdac56e56b4bec928045408ec
Mert <mert@ait.com.tr>
2024-01-09 17:08:02 +0300

    Add new UIX components: basic_checkbox, basic_dialog, basic_select

--- src/uix_components/__init__.py
+++ src/uix_components/__init__.py
@@ -1,2 +1,5 @@
 from ._basic_slider._basic_slider import basic_slider
 from ._component_list._component_list import component_list
+from ._basic_checkbox._basic_checkbox import basic_checkbox
+from ._basic_select._basic_select import basic_select
+from ._basic_dialog._basic_dialog import basic_dialog
