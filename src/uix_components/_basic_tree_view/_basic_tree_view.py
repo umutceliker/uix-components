46f9c4859e83375b8b4768fafa069a895099fac9
mugegngrd <muge@ait.com.tr>
2024-01-18 16:02:43 +0300

    Update basic_tree_view.css and basic_tree_view.py

--- src/uix_components/_basic_tree_view/_basic_tree_view.py
+++ src/uix_components/_basic_tree_view/_basic_tree_view.py
@@ -1,34 +1,32 @@
 import uix
-from uix.elements import unorderedlist, listitem,label, div,check,radio,row,col
-from .._basic_checkbox._basic_checkbox import basic_checkbox
+from uix.elements import unorderedlist, listitem,label, div,check,row
 
 
 uix.html.add_css_file("_basic_tree_view.css",__file__)
-
 
 class basic_tree_view(uix.Element):
     def __init__(self,id, data, main_title):
         super().__init__(id=id)
         if data == None:
             data = {}
+        self.main_title = main_title
 
         def create_tree(key, data, component):
             with listitem(id="li-"+key.lower()).style("list-style","none"):
-                with row("").style("display","flex").style("flex-wrap","wrap"):
-                    label_attributes = {"usefor": "trigger-" + key.lower()}
-                    check(id="trigger-" + key.lower()).cls("comp_type") 
-                    with label(value=key,**label_attributes) as label_:
-                        component()
-                        self.label = label_
-                    if isinstance(data, dict) and data:
-                        self.label.cls("super-class")
-                        with unorderedlist("").cls("pure-tree"):
-                            for sub_key in data:
-                                create_tree(sub_key, data[sub_key], component)
-        
-        with unorderedlist("").cls("pure-tree main-tree"):
-            create_tree(main_title, data, check)
-        
+                label_attributes = {"usefor": "trigger-" + key.lower()}
+                check(id="trigger-" + key.lower()).cls("comp_type") 
+                self.label=label(value=key,**label_attributes)
+                if isinstance(data, dict) and data:
+                    self.label.cls("super-class")
+                    with self.label:
+                        div("").cls("plus").tag="span"
+                    with unorderedlist("").cls("pure-tree"):
+                        for sub_key in data:
+                            create_tree(sub_key, data[sub_key],component)
+        with self:
+            with unorderedlist("").cls("pure-tree main-tree"):
+                create_tree(main_title, data, check)
+
 title="Basic Tree View"
 description="""
 ## basic_tree_view(id, data)
