c77c0ee528d3f37940309aefdfa448f4a0623083
Emre <emre@ait.com.tr>
2024-01-23 13:41:48 +0300

    Refactor basic_datalist class to support additional parameters and improve code readability

--- src/uix_components/_basic_datalist/_basic_datalist.py
+++ src/uix_components/_basic_datalist/_basic_datalist.py
@@ -2,24 +2,34 @@ import uix
 from uix.elements import option, datalist, input
 
 class basic_datalist(uix.Element):
-    def __init__(self, name, value=None, id=None, options=[], callback=None):
-        super().__init__(name, id=id)
+    def __init__(self, name, value=None, id=None, options : dict[str, str] = None, callback=None, placeholder=None, required:bool = False , **kwargs):
+        super().__init__(name, id=id, **kwargs)
         self.options = options
         self.datalistID = id + "-datalist"
         self.inputID = id + "-input"
         self.callback = callback
-        self.cls("border")
+        self.placeholder = placeholder
+        self.required = required
         
         with self:
-            input(value="", id=self.inputID, type="text", list=self.datalistID).on("change", self.on_dlist_change).style("margin-left", "10px;")
-            with datalist(id=self.datalistID):
-                for opt in self.options:
-                    option(value=opt, id=opt)
+            self.input = input(value="", 
+                               id=self.inputID, 
+                               type="text", 
+                               list=self.datalistID, 
+                               placeholder=self.placeholder,
+                               required=self.required).on("change", self.on_dlist_change)
+            
+            with datalist(id=self.datalistID) as self.datalist:
+                if type(self.options) == list:
+                    for _option in self.options:
+                        option(id=_option, value=_option)
+                else:
+                    for key, value in self.options.items():
+                        option(value=value, id=key)
             
     def on_dlist_change(self, ctx, id, value):
         if self.callback:
             self.callback(ctx, id, value)
-        
 title = "Basic Datalist"
 description = """
 # basic_datalist(name, value, id, options[], callback)
