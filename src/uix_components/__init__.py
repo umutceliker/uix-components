43205eaceaf8186c3a67fa9d2221de2a204aadeb
umutceliker <umut@ait.com.tr>
2024-01-25 13:32:34 +0300

    tooltip components eklendi.

--- src/uix_components/__init__.py
+++ src/uix_components/__init__.py
@@ -14,3 +14,4 @@ from ._credit_card._credit_card import credit_card
 from ._image_viewer._image_viewer import image_viewer
 from ._basic_tree_view._basic_tree_view import basic_tree_view
 from ._chart._chart import chart
+from ._tooltip._tooltip import tooltip
