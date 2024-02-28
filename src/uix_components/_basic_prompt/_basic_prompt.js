e63e8da4d66190a9c6946d798d95285095a790b3
semiharslanait <semih@ait.com.tr>
2024-02-28 11:42:16 +0300

    Refactor prompt initialization and add responsive styles

--- src/uix_components/_basic_prompt/_basic_prompt.js
+++ src/uix_components/_basic_prompt/_basic_prompt.js
@@ -1,11 +1,15 @@
-// Check if the event handler is not already set
 if (!event_handlers["init-prompt"]) {
     event_handlers["init-prompt"] = (id, value, event_name) => {
-        const inputElement = document.getElementById("prompt-input");
+        const inputElement = document.getElementById(value.inputID);
         const bottomElement = document.getElementById('bottom');
         const promptElement = document.getElementById('prompt-wrapper');
 
+        console.log("init-prompt", id, value, event_name);
+
         const focusHandler = () => {
+            if(bottomElement === null) {
+                return;
+            }
             bottomElement.classList.remove('hidden');
             document.addEventListener('click', outSideClickHandler);
             inputElement.addEventListener('keydown', keyupHandler);
@@ -22,11 +26,18 @@ if (!event_handlers["init-prompt"]) {
         const keyupHandler = (e) => {
             if (e.key == 'Delete' || e.key == 'Backspace') {
                 if (inputElement.value === '') {
-                clientEmit(value.inputID, "prompt", "pop");
+                    console.log("prompt", "pop");
+                    clientEmit(value.inputID, "prompt", "pop");
                 }
             }
         };
 
         inputElement.addEventListener('focus', focusHandler);
+        // Make sure to call the focus handler initially if needed
+        if (document.activeElement === inputElement) {
+            focusHandler();
+        }
     };
+} else {
+    console.error("init-prompt is already defined");
 }
