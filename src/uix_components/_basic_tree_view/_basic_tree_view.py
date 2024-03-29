bf6b54822677f23d277dedd7a32c68500d8f1759
Mert <mert@ait.com.tr>
2024-03-29 10:30:59 +0300

    Update basic_tree_view.css and _basic_tree_view.py

--- src/uix_components/_basic_tree_view/_basic_tree_view.py
+++ src/uix_components/_basic_tree_view/_basic_tree_view.py
@@ -1,31 +1,32 @@
+from uix.elements import unorderedlist, listitem,label, div,check
 import uix
-from uix.elements import unorderedlist, listitem,label, div,check,row
 
 
 uix.html.add_css_file("_basic_tree_view.css",__file__)
 
 class basic_tree_view(uix.Element):
-    def __init__(self,id, data, main_title):
+    def __init__(self, id, data):
         super().__init__(id=id)
-        if data == None:
+        if data is None:
             data = {}
-        self.main_title = main_title
-
+        
         def create_tree(key, data, component):
             with listitem(id="li-"+key.lower()).style("list-style","none"):
                 label_attributes = {"usefor": "trigger-" + key.lower()}
                 check(id="trigger-" + key.lower()).cls("comp_type") 
-                self.label=label(value=key,**label_attributes)
+                self.label = label(value=key, **label_attributes)
                 if isinstance(data, dict) and data:
                     self.label.cls("super-class")
                     with self.label:
-                        div("").cls("plus").tag="span"
+                        div("").cls("plus").tag = "span"
                     with unorderedlist("").cls("pure-tree"):
                         for sub_key in data:
-                            create_tree(sub_key, data[sub_key],component)
+                            create_tree(sub_key, data[sub_key], component)
+        
         with self:
             with unorderedlist("").cls("pure-tree main-tree"):
-                create_tree(main_title, data, check)
+                for main_title in data:
+                    create_tree(main_title, data[main_title], check)
 
 title="Basic Tree View"
 description="""
@@ -36,12 +37,12 @@ description="""
 | attr                  | desc                                              |
 | :-------------------- | :------------------------------------------------ |
 | id                    | basic_tree_view elementinin id'si                         |
-| main_title                 | ağaç yapısının başlığı. Örnek: "views" |
 | data                  | ağaç yapısının oluşturulması için gerekli veri yapısı. Örnek: {"views":{"view1":{},"view2":{}}} |
 """
 
 sample="""
 data = {
+    "Tree View": {
     "key1": "value1",
     "key2": "value2",
     "key3": {
@@ -54,11 +55,13 @@ data = {
         }
     }
 }
+}
 
 def basic_tree_example():
     with div() as basic_tree_view_example:
-        basic_tree_view(id="basic_tree_view_example",data=data, title="views")
-    return basic_tree_view_example"""
+        basic_tree_view(id="basic_tree_view_example",data=data)
+    return basic_tree_view_example
+"""
 
         
 
